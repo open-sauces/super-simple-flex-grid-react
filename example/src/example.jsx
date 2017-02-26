df31333f096402444b2749f89b6064be007a1b06
Vincent Bollaert <vincentbollaert@gmail.com>
2017-02-26 21:43:51 +0200

    feature: implemented markdown and refactored example

--- example/src/example.jsx
+++ example/src/example.jsx
@@ -2,121 +2,186 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { FlexRow as Row, FlexCell as Col } from '../../src/FlexGrid';
+import Markdown from '../markdown';
+import Aside from './Aside';
+
+import centerMD from '../documentation/center.md';
+import centerHMD from '../documentation/centerH.md';
+import centerVMD from '../documentation/centerV.md';
+import startHMD from '../documentation/startH.md';
+import endHMD from '../documentation/endH.md';
+import startVMD from '../documentation/startV.md';
+import endVMD from '../documentation/endV.md';
+import spaceAroundMD from '../documentation/spaceAround.md';
+import spaceBetweenMD from '../documentation/spaceBetween.md';
+import shrink2ColMD from '../documentation/shrink2Col.md';
+import shrink3ColMD from '../documentation/shrink3Col.md';
+import col2MD from '../documentation/col2.md';
+import noMarginMD from '../documentation/noMargin.md';
+import grow2MD from '../documentation/grow2.md';
+import grow0CustomClassMD from '../documentation/grow0CustomClass.md';
+import ellipsisMD from '../documentation/ellipsis.md';
+import ellipsisGrow2MD from '../documentation/ellipsisGrow2.md';
+import ellipsisNoMarginMD from '../documentation/ellipsisNoMargin.md';
+
 import './style.scss';
 
 const FlexGridExample = () => {
   const customWidth = 'custom-width';
   const customWidth2 = 'custom-width2';
   const ellipsisText = 'Ellipsis ellipsis ellipsis ellipsis ellipsis ellipsis ellipsis ellipsis ellipsis ellipsis';
+  const exampleClass = 'examples__example';
 
   return (
-    <Row customClass="container">
-      <Col customClass="examples">
-        <Row>
-          <Col center><span>center</span></Col>
-        </Row>
+    <div>
+      <Aside />
+      <Row customClass="container">
+        <Col customClass="examples">
+          <div className={exampleClass} data-prop="center" id="center">
+            <Row>
+              <Col center />
+            </Row>
+            <Markdown html={centerMD} />
+          </div>
 
-        <Row>
-          <Col centerH><span>centerH</span></Col>
-        </Row>
+          <div className={exampleClass} data-prop="centerH" id="centerH">
+            <Row>
+              <Col centerH />
+            </Row>
+            <Markdown html={centerHMD} />
+          </div>
 
-        <Row>
-          <Col centerV><span>centerV</span></Col>
-        </Row>
+          <div className={exampleClass} data-prop="centerV" id="centerV">
+            <Row>
+              <Col centerV />
+            </Row>
+            <Markdown html={centerVMD} />
+          </div>
 
-        <Row>
-          <Col startH><span>startH</span></Col>
-        </Row>
+          <div className={exampleClass} data-prop="startH" id="startH">
+            <Row>
+              <Col startH />
+            </Row>
+            <Markdown html={startHMD} />
+          </div>
 
-        <Row>
-          <Col endH><span>endH</span></Col>
-        </Row>
+          <div className={exampleClass} data-prop="endH" id="endH">
+            <Row>
+              <Col endH />
+            </Row>
+            <Markdown html={endHMD} />
+          </div>
 
-        <Row>
-          <Col startV><span>startV</span></Col>
-        </Row>
+          <div className={exampleClass} data-prop="startV" id="startV">
+            <Row>
+              <Col startV />
+            </Row>
+            <Markdown html={startVMD} />
+          </div>
 
-        <Row>
-          <Col endV><span>endV</span></Col>
-        </Row>
+          <div className={exampleClass} data-prop="endV" id="endV">
+            <Row>
+              <Col endV />
+            </Row>
+            <Markdown html={endVMD} />
+          </div>
 
-        <Row>
-          <Col spaceAround><span>spaceAround</span></Col>
-        </Row>
+          <div className={exampleClass} data-prop="spaceAround" id="spaceAround">
+            <Row>
+              <Col spaceAround />
+            </Row>
+            <Markdown html={spaceAroundMD} />
+          </div>
 
-        <Row>
-          <Col spaceBetween><span>spaceBetween</span></Col>
-        </Row>
+          <div className={exampleClass} data-prop="spaceBetween" id="spaceBetween">
+            <Row>
+              <Col spaceBetween />
+            </Row>
+            <Markdown html={spaceBetweenMD} />
+          </div>
 
-        <Row>
-          <Col />
-          <Col shrink><span>shrink</span></Col>
-        </Row>
+          <div className={exampleClass} data-prop="shrink" id="shrink">
+            <Row>
+              <Col />
+              <Col shrink />
+            </Row>
+            <Markdown html={shrink2ColMD} />
+          </div>
 
-        <Row>
-          <Col />
-          <Col shrink><span>shrink</span></Col>
-          <Col />
-        </Row>
+          <div className={exampleClass} data-prop="shrink 2">
+            <Row>
+              <Col />
+              <Col shrink />
+              <Col />
+            </Row>
+            <Markdown html={shrink3ColMD} />
+          </div>
 
-        <Row>
-          <Col />
-          <Col />
-        </Row>
+          <div className={exampleClass}>
+            <Row>
+              <Col />
+              <Col />
+            </Row>
+            <Markdown html={col2MD} />
+          </div>
 
-        <Row>
-          <Col noMargin><span>noMargin</span></Col>
-          <Col noMargin><span>noMargin</span></Col>
-          <Col />
-        </Row>
+          <div className={exampleClass} data-prop="noMargin" id="noMargin">
+            <Row>
+              <Col noMargin />
+              <Col noMargin />
+              <Col />
+            </Row>
+            <Markdown html={noMarginMD} />
+          </div>
 
-        <Row>
-          <Col />
-          <Col grow={2}><span>grow2</span></Col>
-          <Col />
-          <Col />
-        </Row>
+          <div className={exampleClass} data-prop="grow" id="grow">
+            <Row>
+              <Col />
+              <Col grow={2} />
+              <Col />
+              <Col />
+            </Row>
+            <Markdown html={grow2MD} />
+          </div>
 
-        <Row>
-          <Col />
-          <Col grow={0} customClass={customWidth}><span>grow0 | customClass</span></Col>
-          <Col />
-          <Col />
-        </Row>
+          <div className={exampleClass} data-prop="grow, customClass">
+            <Row>
+              <Col />
+              <Col grow={0} customClass={customWidth} />
+              <Col />
+              <Col />
+            </Row>
+            <Markdown html={grow0CustomClassMD} />
+          </div>
 
-        <Row>
-          <Col ellipsis>
-            {ellipsisText}
-            <span>ellipsis</span>
-          </Col>
-          <Col />
-          <Col ellipsis>
-            {ellipsisText}
-            <span>ellipsis</span>
-          </Col>
-        </Row>
+          <div className={exampleClass} data-prop="ellipsis" id="ellipsis">
+            <Row>
+              <Col ellipsis>{ellipsisText}</Col>
+              <Col />
+              <Col ellipsis>{ellipsisText}</Col>
+            </Row>
+            <Markdown html={ellipsisMD} />
+          </div>
 
-        <Row>
-          <Col ellipsis grow={2}>
-            {ellipsisText}
-            <span>ellipsis | grow2</span>
-          </Col>
-          <Col />
-          <Col />
-        </Row>
+          <div className={exampleClass} data-prop="ellipsis, grow">
+            <Row>
+              <Col ellipsis grow={2}>{ellipsisText}</Col>
+              <Col />
+              <Col />
+            </Row>
+            <Markdown html={ellipsisGrow2MD} />
+          </div>
 
-        <Row>
-          <Col ellipsis noMargin>
-            {ellipsisText}
-            <span>ellipsis | noMargin</span>
-          </Col>
-          <Col ellipsis noMargin grow={0} customClass={customWidth2}>
-            {ellipsisText}
-            <span>ellipsis | noMargin | grow0 | customClass</span>
-          </Col>
-        </Row>
-      </Col>
-    </Row>
+          <div className={exampleClass} data-prop="ellipsis, noMargin, grow, customClass">
+            <Row>
+              <Col ellipsis noMargin>{ellipsisText}</Col>
+              <Col ellipsis noMargin grow={0} customClass={customWidth2}>{ellipsisText}</Col>
+            </Row>
+            <Markdown html={ellipsisNoMarginMD} />
+          </div>
+        </Col>
+      </Row>
+    </div>
   );
 };
 
