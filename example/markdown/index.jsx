c1fa2eee7accbb6cce468f73c8646b264f68f05e
Vincent Bollaert <vincentbollaert@gmail.com>
2017-02-26 21:33:02 +0200

    feature: added markdown

---
+++ example/markdown/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { FlexRow as Row, FlexCell as Col } from '../../src/FlexGrid';
+
+import './theme-one-light.css';
+
+const Markdown = (props) => {
+  const html = {
+    __html: props.html,
+  };
+
+  return (
+    <Row>
+      <Col centerV customClass="markdown"><article dangerouslySetInnerHTML={html} /></Col>
+    </Row>
+  );
+};
+
+Markdown.propTypes = {
+  html: React.PropTypes.string,
+};
+
+export default Markdown;
